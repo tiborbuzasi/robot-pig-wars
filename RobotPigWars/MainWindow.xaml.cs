15636c17f39b0cbda0edddc1258fc79a8c4ce606
Tibor Buzási <tib.buzasi@gmail.com>
2019-03-26 00:05:54 +0100

    Barebone GUI layout added
    - Basic WPF project added
    - GUI layout defined
    - Window actions added (Drag, Minimize, Maximize, Close)
    - OSD buttons layout defined

---
+++ RobotPigWars/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotPigWars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowTitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+
+            if (e.ClickCount == 2)
+            {
+                WindowState = (WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+            }
+        }
+
+        private void WindowCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void WindowMaximazeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void WindowMinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Minimized) ? WindowState.Normal : WindowState.Minimized;
+        }
+
+        private void WindowHelpButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WindowSettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WindowSaveGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WindowLoadGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WindowNewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OsdArrow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
+
